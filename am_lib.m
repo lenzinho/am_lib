72b60a9c19344c468294451e0a6040f6d0ba773c
lenzinho <lenzinho@lenzinhos-MacBook-Pro-2.local>
2018-01-21 22:32:40 -0500

    distance transform

--- am_lib.m
+++ am_lib.m
@@ -2753,6 +2753,115 @@ classdef am_lib
             end
         end
 
+        
+        % interesting transforms        
+
+        function [D R] = DT(img)
+            % Two-dimensional generalized distance transform
+            %
+            % Input: f - the sampled function
+            %
+            % Output: D - distance transform
+            %         R - power diagram
+            %
+            % Based on the paper:
+            % P. Felzenszwalb, D. Huttenlocher
+            % Distance Transforms of Sampled Functions
+            % Cornell Computing and Information Science Technical Report TR2004-1963,
+            % September 2004
+            %
+            %
+            % This is a simple MATLAB implmentation of the generalized distance
+            % transform algorithm. The function DT() gives the distance transform 
+            % of a 2D image by calling DT1() for each dimension. By using DT1(), 
+            % this could be easily extended to higher dimensions. It seems to have 
+            % problems with inf values, so for points in the image with "no" parabola 
+            % centered there, they should instead be given a large numeric value 
+            % (such as 1e10). I also modified the algorithm so that the second argument 
+            % returns the power diagram of the input. The power diagram is a diagram 
+            % where each point is assigned to the point that is closest to it with 
+            % respect to the distance transform. If all input points have the same 
+            % value, this function reduces to giving the standard distance transform 
+            % and the Voronoi diagram.
+            % 
+            % % EXAMPLE:
+            % % 
+            % % Create some random points
+            % X = randi(100,50,2);
+            % % Create an image with rando values at these points
+            % img = sparse(X(:,2), X(:,1), rand(50,1)*20,100,100);
+            % % Set all other values to a high number
+            % img(img==0) = 1e10;
+            % % Call the function
+            % [D R] = DT(img);
+            % % Plot the results
+            % figure;
+            % subplot(1,2,1);
+            % imagesc(D);
+            % title('Generalized Distance transform');
+            % axis image;
+            % subplot(1,2,2);
+            % imagesc(R);
+            % title('Power diagram');
+            % axis image;
+
+            D = zeros(size(img));
+            R = zeros(size(img));
+
+            for i = 1:size(img,1)
+                [d r] = DT1(img(i,:));
+                R(i,:) = r;
+                D(i,:) = d;
+            end
+            for j = 1:size(img,2)
+                [d r] = DT1(D(:,j));
+                D(:,j) = d;
+                R(:,j) = sub2ind(size(img), r, R(r,j));
+            end
+            function [D R] = DT1(f)
+                % One-dimensional generalized distance transform
+                %
+                % Input: f - the sampled function
+                %
+                % Output: D - distance transform
+                %         R - power diagram
+                %
+                % Based on the paper:
+                % P. Felzenszwalb, D. Huttenlocher
+                % Distance Transforms of Sampled Functions
+                % Cornell Computing and Information Science Technical Report TR2004-1963, September 2004
+
+                n = numel(f);
+                D = zeros(n,1);
+                R = zeros(n,1);
+
+                k = 1; % Index of the rightmost parabola in the lower envelope
+                v = ones(n,1); % Locations of the parabolas in the lower envelope
+                z = ones(n,1); %Locations of boundaries between parabolas
+                z(1) = -inf;
+                z(2) = inf;
+
+                for q = 2:n
+                    s = ((f(q) + q^2) - (f(v(k)) + v(k)^2))/(2*q - 2*v(k));
+                    while s <= z(k)
+                        k = k - 1;
+                        s = ((f(q) + q^2) - (f(v(k)) + v(k)^2))/(2*q - 2*v(k));
+                    end
+                    k = k + 1;
+                    v(k) = q;
+                    z(k) = s;
+                    z(k+1) = inf;
+                end
+                k = 1;
+                for q = 1:n
+                    while z(k+1) < q
+                        k = k+1;
+                    end
+                    D(q) = (q-v(k))^2 + f(v(k));
+                    R(q) = v(k);
+                end
+            end
+        end
 
         % vector calculous
         
